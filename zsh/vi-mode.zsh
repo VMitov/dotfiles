8e57e3077ce286cd6c1c7fdaa330361c2d5cfa0c
Vladislav Mitov <vladislav.mitov@gmail.com>
2021-02-12 11:33:18 +0200

    Migrate to latest p10k and remove obsolete zsh configurations

--- zsh/vi-mode.zsh
+++
@@ -1,9 +0,0 @@
-bindkey -v
-bindkey -a 's' vi-insert
-bindkey -a 'S' vi-insert-bol
-bindkey -a 't' vi-add-next
-bindkey -a 'T' vi-add-eol
-bindkey -a 'l' vi-backward-word
-bindkey -a 'u' vi-forward-blank-word-end
-bindkey -a 'y' vi-forward-word
-bindkey -a 'z' vi-undo-change
